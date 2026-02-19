079241b261fee9d36ee018918e75d26fa024636d
ztlan <ztlan@login.cl.upenn.edu>
2026-02-18 20:17:54 -0500

    update hf repo for sam3

--- sam3/model_builder.py
+++ sam3/model_builder.py
@@ -642,7 +642,7 @@ def build_sam3_image_model(
 
 
 def download_ckpt_from_hf():
-    SAM3_MODEL_ID = "jetjodh/sam3"
+    SAM3_MODEL_ID = "facebook/sam3"
     SAM3_CKPT_NAME = "sam3.pt"
     SAM3_CFG_NAME = "config.json"
     _ = hf_hub_download(repo_id=SAM3_MODEL_ID, filename=SAM3_CFG_NAME)
